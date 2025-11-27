6aff82641b57e7e5cde9c0ad1e673c816e0dbee1
besirarikk <arikkbesir@gmail.com>
2025-11-27 20:58:09 +0300

    fix: remove native Firebase configure from iOS AppDelegate

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,33 +1,13 @@
 import UIKit
 import Flutter
-import FirebaseCore
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    // Firebase init (varsa)
-    FirebaseApp.configure()
-
-    // TÜM Flutter pluginleri için tek ve doğru kayıt noktası
     GeneratedPluginRegistrant.register(with: self)
-
-    // Üst sınıfın kendi işini yapmasına izin ver
-    return super.application(
-      application,
-      didFinishLaunchingWithOptions: launchOptions
-    )
-  }
-
-  override func application(
-    _ app: UIApplication,
-    open url: URL,
-    options: [UIApplication.OpenURLOptionsKey : Any] = [:]
-  ) -> Bool {
-    // url_launcher, sign-in vb. için
-    return super.application(app, open: url, options: options)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
