f3c5f9f908e5ce5108c183cb1e8ea29d03e70715
besirarikk <arikkbesir@gmail.com>
2025-12-30 01:42:31 +0300

    update

--- ios/Runner/SyraNativeBlurView.swift
+++ ios/Runner/SyraNativeBlurView.swift
@@ -5,10 +5,26 @@ final class SyraNativeBlurView: NSObject, FlutterPlatformView {
   private let blurView: UIVisualEffectView
 
   init(frame: CGRect, viewId: Int64, args: Any?) {
-    let effect = UIBlurEffect(style: .systemThinMaterialDark)
+    // Try different blur styles:
+    // .systemUltraThinMaterialDark - en hafif
+    // .systemThinMaterialDark - hafif
+    // .systemMaterialDark - orta
+    // .systemThickMaterialDark - yoğun
+    // .systemChromeMaterialDark - en yoğun
+    // .dark - klasik iOS blur
+    
+    let effect = UIBlurEffect(style: .dark)
     blurView = UIVisualEffectView(effect: effect)
     blurView.frame = frame
     blurView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+    
+    // Blur'un üstüne hafif siyah tint ekle
+    let tintView = UIView()
+    tintView.backgroundColor = UIColor.black.withAlphaComponent(0.3)
+    tintView.frame = blurView.bounds
+    tintView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+    blurView.contentView.addSubview(tintView)
+    
     super.init()
   }
 
